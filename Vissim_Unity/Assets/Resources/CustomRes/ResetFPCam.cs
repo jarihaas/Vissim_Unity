bc05c01d42dd62051c25e4353aee28872ccaf64a
jarihaas <jarihaas@gmail.com>
2026-08-24 17:08:27 -0300

    Add scene objects, plugins, mocap data and resources

---
+++ Vissim_Unity/Assets/Resources/CustomRes/ResetFPCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ResetFPCam : MonoBehaviour
+{
+  public void ResetCam()
+  {
+	Vector3 carPos = GameObject.Find("SkyCar").transform.position;
+		Vector3 headsetOffset = InputTracking.GetLocalPosition(XRNode.Head);
+		Vector3 driverPosition = new Vector3((carPos.x - 0.4f) - headsetOffset.x, (carPos.y + 1.2f) - headsetOffset.y, (carPos.z - 0.19f) - headsetOffset.z);
+		gameObject.transform.position = driverPosition;
+
+  }
+}
